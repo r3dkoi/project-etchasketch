689cc411430756d79c92fb399c48cd555569cbb6
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-01-18 10:35:46 +0800

    16x16 grid made  but need flexbox

--- sketchpadscript.js
+++ sketchpadscript.js
@@ -1,7 +1,11 @@
-document.querySelector(".container").innerHTML = "";
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".container");
+    container.innerHTML= "";
     for (let rows = 0; rows < 16; rows++) {
         for (let columns = 0; columns < 16; columns++){
             let div = document.createElement("div");
-            document.querySelector(".container").appendChild(div);
+            container.appendChild(div);
         }
     }
+
+});
