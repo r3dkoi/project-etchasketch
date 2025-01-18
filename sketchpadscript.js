bc64d8d23625134baebb552d1473ac50b7ccf133
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-01-18 12:44:05 +0800

    added new reference to div to make it easier

--- sketchpadscript.js
+++ sketchpadscript.js
@@ -4,15 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let rows = 0; rows < 16; rows++) {
         for (let columns = 0; columns < 16; columns++){
             let div = document.createElement("div");
+            div.classList.add("grid-item");
             container.appendChild(div);
         }
     }
 
 });
 
-document.addEventListener("mouseover", function (event) {
-    const div = event.target.closest(".container div");
-    if (div) {
-        div.style.color = "red";
-    }
-});
