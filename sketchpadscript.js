a184749dcf4a9fa81a2ed6b37fc1c0ea0693b6d7
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-01-19 09:46:49 +0800

    inputted variables for new rows and columns

--- sketchpadscript.js
+++ sketchpadscript.js
@@ -14,5 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
             div.style.background = "red";
         }
     });
+    const button = document.querySelector(".reset");
+    button.addEventListener("onclick", function () {
+        const userRowsInput = prompt("How many rows do you want your sketchpad to have?");
+        const userColumnsInput = prompt("How many columns do you want your sketchpad to have?");
+        let userRows = parseInt(userRowsInput, 10); 
+        let userColumns = parseInt(userColumnsInput, 10);
+        // converts user input into integers for following IF
+
+        if ((isNaN(userRows)) || (isNaN(userColumns)) || 
+            (userRows > 100) || (userColumns > 100)) {
+                
+            }
+
+    });
 });
+
+
+
+
 
